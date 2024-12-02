2bae2e9e0fd05e26a5599e678edbb112dbb2a3ef
Imad <imadchowdhury01@gmail.com>
2024-12-02 21:50:13 +0000

    init commit of exisiting repo

---
+++ day_1/day_1_main.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+## Task 1
+list_1 = []
+list_2 = []
+
+with open("day_1/input.txt", "r") as f:
+    for line in f:
+        pair = line.strip('\n').split('   ')
+        list_1.append(int(pair[0]))
+        list_2.append(int(pair[1]))
+
+list_1 = sorted(list_1)
+list_2 = sorted(list_2)
+
+total_difference = 0
+for v1, v2 in zip(list_1, list_2):
+    total_difference += abs(v1 - v2)
+
+print(f'Task 1: {total_difference}')
+
+## Task 2
+count_dict = Counter(list_2)
+
+similarity_score = 0
+for i in list_1:
+    if i in count_dict:
+        similarity_score += i * count_dict[i]
+
+print(f'Task 2: {similarity_score}')
---
+++ day_2/day_2_main.py
@@ -0,0 +1,94 @@
+def check_consistent_direction(list_):
+    # is list increasing or decreasing?
+    if list_[0] < list_[1]:
+        increasing = True
+    elif list_[0] > list_[1]:
+        increasing = False
+    else: 
+        return False
+
+    # check direction is consistent
+    for i, item in enumerate(list_):
+        # dont want to test last element
+        if i == len(list_) - 1:
+            break
+
+        if increasing:
+            if item > list_[i+1]:
+                return False
+        else: # then decreasing
+            if item < list_[i+1]:
+                return False
+
+    return True
+
+
+def check_constrained_difference(list_):
+    for i, item in enumerate(list_):
+        # dont want to test first element
+        if i == 0:
+            continue
+
+        # is difference between constraints
+        difference = abs(item - list_[i-1])
+        if (difference < 1) or (difference > 3):
+            return False
+        
+    return True
+
+
+# get all reports
+all_reports = []
+with open("day_2/input.txt", "r") as f:
+    for line in f:
+        report = line.strip('\n').split(' ')
+        report = [int(level) for level in report]
+        all_reports.append(report)
+ 
+
+# Task 1: assess each report for saftey
+safe_reports = 0
+for report in all_reports:
+    safe = True # default is safe, will check below
+
+    safe = check_consistent_direction(report) and\
+        check_constrained_difference(report)
+
+    # add to tally if still safe
+    if safe: 
+        safe_reports += 1
+
+print(f'Task 1: {safe_reports}')
+
+# Task 2: with saftey dampener
+# generate a list of lists 
+# of all possible combinations for each report
+all_reports_combos = []
+for report in all_reports:
+    report_combo = []
+    for i in range(0, len(report)):
+        report_combo.append(report[:i]+report[i+1:])
+
+    all_reports_combos.append(report_combo)
+
+
+safe_reports = 0
+# loop through all combos for each report
+for report_combo in all_reports_combos:
+    combo_safe = False # will assume no combo is safe, prove otherwise below
+    for report in report_combo:
+        # (same process as for Task 1)
+        safe = True # default is safe, will check below
+
+        safe = check_consistent_direction(report) and\
+            check_constrained_difference(report)
+        
+        if safe: 
+            combo_safe = True
+            break # no need to test all combos if one is already safe
+
+    # add to tally if at least one in the combo is safe
+    if combo_safe == True:
+        safe_reports += 1
+
+print(f'Task 2: {safe_reports}')
